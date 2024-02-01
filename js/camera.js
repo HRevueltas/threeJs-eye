026cc728f0d42fddfbbd56a5162252fcd093b96f
Harold Revueltas Gómez <105690550+HRevueltas@users.noreply.github.com>
2024-02-01 18:13:56 -0500

    object follow u

--- js/camera.js
+++ js/camera.js
@@ -1,6 +1,9 @@
 import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
 import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
 import { gsap } from "https://cdn.skypack.dev/gsap";
+import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+
+
 
 // Cursor
 const cursor = {
@@ -8,53 +11,24 @@ const cursor = {
     y: 0
 }
 
-window.addEventListener('mousemove', (event) => {
-
-    
-    cursor.x = event.clientX / sizes.width - 0.5;
-    cursor.y = -(event.clientY / sizes.height - 0.5);
-
-    // Update the mesh position based on cursor position
-    gsap.to(mesh.position, {
-        duration: 1,
-        x: cursor.x * 5,
-        y: cursor.y * 5,
-        z: 0,
-        // ease: "bounce" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
-        // ease: "elastic" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
-        // ease: "back" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
-        // ease: "steps(600)" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
-        // ease: "power4" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
-        // ease: "power2" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
-        // ease: "power1" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
-        // ease: "power3" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
-        // ease: "power5" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
-        // ease: "power6.out" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
-        ease: "expo" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
-        // ease: "expo" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
-        // ease: "sine" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
-        // ease: "linear" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
-        // ease: "quint" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
-        // ease: "quart" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
-        // ease: "quad" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
-        // ease: "cubic" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
-         
-    });
-
-    // Update the mesh rotation based on cursor position
-     
-    gsap.to(mesh.rotation, {
-        duration: 1,
-        x: cursor.y * 5,
-        y: cursor.x * 5,
-
-        ease: "elastic" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
-    });
-});
 
 
 // Scene
 const scene = new THREE.Scene();
+
+// Sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+let { width, height } = sizes;
+
+// camera 
+const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
+// Camera
+camera.position.z = 3;
+scene.add(camera);
+
 
 // Objects
 const geometry = new THREE.BoxGeometry(1, 1, 1);
@@ -72,12 +46,7 @@ scene.add(mesh);
 // Canvas
 const canvas = document.querySelector('.webgl');
 
-// Sizes
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
-let { width, height } = sizes;
+
 
 // Resize event
 window.addEventListener('resize', () => {
@@ -100,30 +69,108 @@ window.addEventListener('dblclick', () => {
     }
 })
 
-// Camera
-const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
-camera.position.z = 3;
-scene.add(camera);
 
 // Controls
 // const controls = new OrbitControls(camera, canvas)
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(width, height);
 
+
+
+
+
+// this indicates if the mouse is over the canvas
+let isMouseOver = true;
+const initialRotation = { x: mesh.rotation.x, y: mesh.rotation.y, z: mesh.rotation.z };
+const initialPosition = { x: mesh.position.x, y: mesh.position.y, z: mesh.position.z };
+window.addEventListener('mousemove', (event) => {
+
+    // Update the cursor object with the mouse position
+    isMouseOver = true;
+    cursor.x = event.clientX / sizes.width - 0.5;
+    cursor.y = -(event.clientY / sizes.height - 0.5);
+
+    // Update the mesh position based on cursor position
+    gsap.to(mesh.position, {
+        duration: 1,
+        x: cursor.x * 5,
+        y: cursor.y * 5,
+        z: 0,
+        // ease: "bounce" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
+        // ease: "elastic" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
+        // ease: "back" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
+        // ease: "steps(600)" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
+        // ease: "power4" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
+        // ease: "power2" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
+        // ease: "power1" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
+        // ease: "power3" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
+        // ease: "power5" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
+        // ease: "power6.out" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
+        // ease: "expo" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
+        // ease: "expo" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
+        // ease: "sine" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
+        // ease: "linear" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
+        // ease: "quint" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
+        // ease: "quart" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
+        // ease: "quad" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
+        // ease: "cubic" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
+
+    });
+
+    // Update the mesh rotation based on cursor position
+    gsap.to(mesh.rotation, {
+        duration: 1,
+        x: cursor.y * 1.4,
+        y: cursor.x * 1.4,
+
+        ease: "back" // Opcional: puedes ajustar el tipo de suavizado según tu preferencia
+    });
+});
+
+// Add a listener to the window, so we can resize the window and the camera
+window.addEventListener("resize", function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+window.addEventListener('mouseout', () => {
+    isMouseOver = false;
+    gsap.to(mesh.position, {
+        duration: 1,
+        x: initialPosition.x,
+        y: initialPosition.y,
+        z: initialPosition.z,
+        ease: "expo"
+    });
+
+ gsap.to(mesh.rotation, {
+        duration: 1,
+        x: initialRotation.x,
+        y: initialRotation.x,
+        z: initialRotation.z,
+        ease: "back"
+    });
+})
+
+
+
+
+
 // Clock
 const clock = new THREE.Clock();
-
 // Animation loop
 const tick = () => {
-    const elapsedTime = clock.getElapsedTime();
+    // const elapsedTime = clock.getElapsedTime();
 
-    // Update objects
-    // mesh.rotation.y = cursor.x * 1.5;
-    // mesh.rotation.x = cursor.y * 2.5;
-    mesh.rotation.y = cursor.x ;
-    mesh.rotation.x = cursor.y ;
 
-    
+    // this will rotate the mesh if the mouse is over the canvas
+    // if (isMouseOver) {
+    //     mesh.rotation.y = cursor.x;
+    //     mesh.rotation.x = cursor.y;
+    // }
+
+
 
     // Update camera
     // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
@@ -135,41 +182,7 @@ const tick = () => {
 
     // Render
     renderer.render(scene, camera);
-
     requestAnimationFrame(tick);
 }
+
 tick();
-window.addEventListener('mouseout', () => {
-    gsap.to(mesh.position, {
-        duration: 1,
-        x: 0,
-        y: 0,
-        ease: "circ"
-    });
-    gsap.to(mesh.rotation, {
-        duration: 1,
-        x:0,
-        y:0,
-        z: 0,
-        // ease: "elastic"
-    });
-})
-
-window.addEventListener('mouseover', () => {
-
-
-    gsap.to(mesh.rotation, {
-        duration: 1,
-        x: 0,
-        y: 0,
-        z: 0,
-        ease: "elastic"
-    });
-    gsap.to(mesh.position, {
-        duration: 2,
-        x: 0,
-        y: 0,
-        // z: 0,
-        ease: "elastic"
-    });
-})
